cc3037423651cb1e92d3f013a8a37c7e8800192c
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2024-10-15 15:35:26 +0200

    NAVBAR ADDED

--- src/App.vue
+++ src/App.vue
@@ -23,22 +23,37 @@ const handleSignout = async () => {
 
 <template>
   <div>
-    <nav class="">
-      <router-link to="/">Home</router-link> |
-      <!-- Only show SecondPage link if the user is logged in -->
-      <router-link to="/secondpage" v-if="isLoggedIn">Second Page</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/login" v-if="!isLoggedIn">Login</router-link> |
-      <router-link to="/register" v-if="!isLoggedIn">Register</router-link> |
-      <!-- Show Sign Out button only if the user is logged in -->
+    <div class="navbar bg-base-100">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+        <li><a><router-link to="/">Home</router-link></a></li>
+        <li><a><router-link to="/secondpage" v-if="isLoggedIn">Second Page</router-link> </a></li>
+        <li><a><router-link to="/about">About</router-link></a></li>
+        <li><a><router-link to="/login" v-if="!isLoggedIn">Login</router-link></a></li>
+        <li><a><router-link to="/register" v-if="!isLoggedIn">Register</router-link></a></li>
+        <li><a>      <!-- Show Sign Out button only if the user is logged in -->
       <button class="dark:bg-pink-900 dark:text-slate-100 bg-emerald-800 text-slate-100 px-4 py-2 rounded-lg"
         @click="handleSignout" v-if="isLoggedIn">
         Sign Out
-      </button>
-      <div class="nav">
+      </button></a></li>
+            <li><a>    <div class="nav">
         <ThemeToggle />
         <!-- Display the ThemeToggle component -->
-      </div>
+      </div></a></li>
+    </ul>
+  </div>
+</div>
+    <nav class="">
+       |
+      <!-- Only show SecondPage link if the user is logged in -->
+      |
+       |
+       |
+       |
+
     </nav>
     <!-- Pass user data to SecondPage -->
     <router-view :user="auth.currentUser"></router-view><!-- Displays the active component -->
