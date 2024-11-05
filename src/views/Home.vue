a277c68c4265bf6c90804b87f90806dbf9f7ca30
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2024-11-05 16:31:24 +0100

    update register details

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,16 +17,16 @@ export default {
     <!-- Right div for text (2/3 width on small and above screens) -->
     <div class="sm:w-2/3 w-full mt-8 px-8 mb-24 md:mb-0">
       <div class="text-[1.5rem] font-bold dark:text-black text-white ">
-        <h1 class="font-bold">SWIZZai - Homepage Welcomes Yalls!</h1>
+        <h1 class="font-black">SWIZZai - Onechannel Chatspace</h1>
         <p class="text-base text-yellow-400 dark:text-indigo-800">Welcome to this chatspace, featuring vue3 and firebase</p>  
       </div>
       <p class="dark:text-gray-900 text-slate-100 text-base italic mt-4">
 Heyhey all,<br />
-In this small web-application, i provide a simple chat-page where authenticated users can exchange messages , talk about this n that, and jut exchange humen interactions.      </p>
+In this small web-application, I provide a free, simple chatspace where authenticated users can exchange messages, talk about this n' that, and just exchange pleasantries or debate wildly.</p>
       <p class="dark:text-gray-900 text-slate-100 text-base italic my-4">
      </p><h3 class="text-base font-bold">How to use:</h3> 
       <p class="dark:text-gray-900 text-slate-100 text-base italic mb-4">
-First time visitors, please use the register link in the navbar or in the login mask to get to the registration form. there you can fill in a displayname (username), we need a e-mail address. You can use a fantasy mail address or sign up/in with Google</p>
+First time visitors, please use the <a class="underline font-bold" href="./register">register</a> link in the navbar or in the login mask to get to the registration form. Fill in a displayname (username), you use an e-mail address. You can use a fantasy mail address or sign up/in with Google.</p>
     </div>  
   </div>
 </div>
