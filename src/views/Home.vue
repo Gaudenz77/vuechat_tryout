17cd274a7eecdbf6cfff2a9a0f1f9b708d53b7b5
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2024-11-05 16:17:55 +0100

    update img ai home.vue added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,17 +8,17 @@ export default {
 }
 </script>
 <template>
-<div>
+<div class="mb-8">
   <div class="flex flex-wrap w-full justify-center my-4 dark:bg-yellow-300 bg-blue-950 rounded-lg px-4 py-2">
 <!-- Left div for image (1/3 width on small and above screens) -->
-    <div class="sm:w-1/3 w-full mb-4 sm:mb-0">
-      <img src="/assets/img/hero_01.png" alt="Placeholder Image" class="w-full h-auto object-cover rounded-md  sm:m-2 md:m-4  sm:p-2 md:p-4" />
+    <div class="sm:w-1/3 w-full mb-4 sm:mb-0 pt-6 md:pt-4">
+      <img src="/assets/img/swizzchat_illu_01_chibi.png" alt="Placeholder Image" class="w-full h-auto object-cover rounded-md  sm:m-2 md:m-4  sm:p-2 md:p-4" />
     </div>
     <!-- Right div for text (2/3 width on small and above screens) -->
-    <div class="sm:w-2/3 w-full mt-8 px-8">
+    <div class="sm:w-2/3 w-full mt-8 px-8 mb-24 md:mb-0">
       <div class="text-[1.5rem] font-bold dark:text-black text-white ">
-        <h1>HSWIZZai - Homepage Welcomes Yalls!</h1>
-        <p class="text-base text-yellow-400 dark:text-indigo-800">Welcome to this chatspace, feuturing vue3 and firebase</p>  
+        <h1 class="font-bold">SWIZZai - Homepage Welcomes Yalls!</h1>
+        <p class="text-base text-yellow-400 dark:text-indigo-800">Welcome to this chatspace, featuring vue3 and firebase</p>  
       </div>
       <p class="dark:text-gray-900 text-slate-100 text-base italic mt-4">
 Heyhey all,<br />
